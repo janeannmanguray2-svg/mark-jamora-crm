2a6b4a321a2fe304d06db5af12fbbb2b13d44d21
jjaamm <254062093+janeannmanguray2-svg@users.noreply.github.com>
2026-07-13 07:54:35 +0800

    Delete public directory

--- public/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-// supabase-js is loaded globally via the CDN <script> tag in index.html
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: true, autoRefreshToken: true }
-});
